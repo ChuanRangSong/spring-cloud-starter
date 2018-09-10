a82412c6182de937ba90430f781cdb3fa706da3c
song <scr0419@outlook.com>
2018-09-10 18:20:49 +0800

    初始化

---
+++ user/src/main/java/com/alibaba/user/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.alibaba.user.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping("/{id}")
+    public void findById(@PathVariable("id") int id){
+        logger.info("id:{}", id);
+    }
+
+}
